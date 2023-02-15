cd0eaf5cd5247ce941d5f8191d65c5afbf2339f8
snkhan47 <khansn47@gmail.com>
2023-02-15 13:38:37 +0530

    First Commit

---
+++ src/main/java/com/nihal/server/repo/ServerRepo.java
@@ -0,0 +1,8 @@
+package com.nihal.server.repo;
+
+import com.nihal.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
---
+++ src/main/java/com/nihal/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,55 @@
+package com.nihal.server.service.implementation;
+
+import com.nihal.server.model.Server;
+import com.nihal.server.repo.ServerRepo;
+import com.nihal.server.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+    private final ServerRepo serverRepo;
+    @Override
+    public Server create(Server server) {
+        log.info("Saving new Server: {}",server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
+    }
+
+
+    @Override
+    public Server ping(String ipAddress) {
+        
+        return null;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public Server get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
+        return null;
+    }
+}
